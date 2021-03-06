2437486d20862f545fa559c8d6d315e2d92cad47
walterhu <alterhu2020@gmail.com>
2021-03-06 21:21:18 +0800

    feat: standard java command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-JVM_ARG=""
-APP_PROP=""
+# ================================================================================
+JVM_ARGS="-server -Xms2g -Xmx2g -Xmn512m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=256m -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseG1GC -XX:+PrintGCDetails -Xlog:gc:gc.log -XX:+HeapDumpOnOutOfMemoryError"
+APP_PROPS="-Djava.security.egd=file:/dev/./urandom"
+# SPRINGBOOT_ARGS="--spring.config.location=/opt/ --logging.config=/opt/logback.xml"
 
+java "${JVM_ARGS}" "${APP_PROPS}" -jar /opt/app.jar
+#java "${JVM_ARGS}" "${APP_PROPS}" -jar /opt/app.jar "${SPRINGBOOT_ARGS}"
