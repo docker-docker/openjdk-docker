229a1cd29ad73085b3634af29735f19ba32e42d0
walterhu <alterhu2020@gmail.com>
2021-03-09 00:02:44 +0800

    test: add more options

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # ================================================================================
 JVM_ARGS="-server -Xms2g -Xmx2g -Xmn512m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=256m -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseG1GC -XX:+PrintGCDetails -Xlog:gc:gc.log -XX:+HeapDumpOnOutOfMemoryError"
 APP_PROPS="-Djava.security.egd=file:/dev/./urandom"
-# SPRINGBOOT_ARGS="--spring.config.location=/opt/ --logging.config=/opt/logback.xml"
+# SPRINGBOOT_ARGS="--loader.path=/opt/app/lib --spring.config.location=/opt/app/ --logging.config=/opt/app/logback.xml"
 
 java "${JVM_ARGS}" "${APP_PROPS}" -jar /opt/app.jar
 # java "${JVM_ARGS}" "${APP_PROPS}" -jar /opt/app.jar "${SPRINGBOOT_ARGS}"
